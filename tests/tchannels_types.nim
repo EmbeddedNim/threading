a325e6228180e62d799d0657523f7cd77ff973bf
Jaremy J. Creechley <jaremy.creechley@panthalassa.com>
2022-06-15 17:57:50 -0700

    testing basic types

---
+++ tests/tchannels_types.nim
@@ -0,0 +1,96 @@
+discard """
+  matrix: "--threads:on --gc:orc; --threads:on --gc:arc"
+  disabled: "freebsd"
+"""
+
+import threading/channels
+import std/[os, logging, unittest]
+import common
+
+
+suite "testing ints":
+  test "int send recv":
+    var chan = newChan[int]()
+    let count = 100
+    var dest = 0
+    for i in 1..count:
+      let msg = i
+      logger.log(lvlDebug, "[main] Send msg: " & $msg)
+      chan.send(msg)
+      chan.recv(dest)
+    logger.log(lvlDebug, "[main] Received msg: " & $dest)
+    doAssert dest == count
+
+  test "int send recvAll":
+    var chan = newChan[int]()
+    let count = 10
+    for i in 1..count:
+      let msg = i
+      logger.log(lvlDebug, "[main] Send msg: " & $msg)
+      chan.send(msg)
+
+    var dest: seq[int]
+    chan.recvAll(dest)
+    logger.log(lvlDebug, "[main] Received msg: " & $dest)
+    doAssert dest.len() == count
+    doAssert dest[^1] == count
+    doAssert dest.len() == count
+
+  test "int reset":
+    var chan = newChan[int]()
+    let count = 10
+    for i in 1..count:
+      let msg = i
+      logger.log(lvlDebug, "[main] Send msg: " & $msg)
+      chan.send(msg)
+
+    check chan.peek() == count
+    chan.clear()
+    check chan.peek() == 0
+
+suite "testing seq":
+  
+  setup:
+    discard "run before each test"
+
+  test "basic init tests":
+    block:
+      let chan0 = newChan[seq[int]]()
+      let chan1 = chan0
+      block:
+        let chan3 = chan0
+        let chan4 = chan0
+
+  test "seq[int] send recv":
+    var chan = newChan[seq[int]]()
+    let count = 100
+    var dest: seq[int]
+    for i in 1..count:
+      let msg = @[i, i, i]
+      logger.log(lvlDebug, "[main] Send msg: " & $msg)
+      chan.send(msg)
+
+      chan.recv(dest)
+      logger.log(lvlDebug, "[main] Received msg: " & $dest)
+
+    doAssert dest.len() == 3
+    doAssert dest[0] == count
+
+  test "seq[int] send recvAll":
+    var chan = newChan[seq[int]]()
+    let count = 10
+    for i in 1..count:
+      let msg = @[i, i, i]
+      logger.log(lvlDebug, "[main] Send msg: " & $msg)
+      chan.send(msg)
+
+    var dest: seq[seq[int]]
+    chan.recvAll(dest)
+    logger.log(lvlDebug, "[main] Received msg: " & $dest)
+    doAssert dest.len() == count
+    doAssert dest[^1].len() == 3
+    doAssert dest[^1][0] == count
+
+  test "seq[int] reset":
+    var chan = newChan[seq[int]]()
+    # runBasicReset(chan, 10)
